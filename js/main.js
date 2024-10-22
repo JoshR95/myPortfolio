c93c2bc6752a33e90d2d900dae9fd51216d3f60d
JoshR95 <joshuarickards@hotmail.co.uk>
2024-10-22 14:14:49 +0100

    3rd - created the type writer js function 22/10/24 - 14:14
    created the type writer js function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-window.onload = function () {
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+/// SETTING IS-ACTIVE FOR THE HAMBURGER AND DROPDOWN
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+document.addEventListener('DOMContentLoaded', function() {
     // this code is adding .hamburger to menu_btn
     const menu_btn = document.querySelector('.hamburger');
 
@@ -10,4 +14,27 @@ window.onload = function () {
         menu.classList.toggle('is-active');
     });
 
-}
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+/// TYPEWRITER EFFECT FOR H1
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+document.addEventListener('DOMContentLoaded', function() {
+    const typewriter = document.getElementById('typewriter'); // this gets the element and it asigns it a variable
+    const text = typewriter.textContent; // this returns all the text inside the element typewriter and asigns it
+    const speed = 200; // how fast it types
+    let i = 0; // empty variable to be counted on 
+
+    typewriter.textContent = ''; // This clears the initial text
+
+    function typeWriter() {
+        if (i < text.length) { // here were checking if the words is done yet 
+            typewriter.textContent += text.charAt(i); // here we add the current letter were on to typewriters (h1) content
+            i++; // this increments the counter
+            setTimeout(typeWriter, speed); // this sets the timer to each letter being added
+        }
+    }
+
+    typeWriter(); // this calls the function to make the text appear on load
+});
